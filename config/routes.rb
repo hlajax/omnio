80ff0bbe1affc3cc06db0f02a765052d0dbcf8bc
henry lepa malomalo <hlepa64@gmail.com>
2020-11-24 10:48:22 +0100

    omnicassetete

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resource :home
+  root 'home#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
